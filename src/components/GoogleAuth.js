5d773179ce5b3d3e21487e1ff08786376373727c
rudyolsson <rudy@flexper.fr>
2019-08-09 13:15:03 +0200

    add auth callback

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = {
+        isSignedIn: null,
+    };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
+            // initialize the auth2 client. Init returns a promise
             window.gapi.client.init({
-                clientId: '148525925888-rdbh1io0jbii70ped8pfied88ki2kjlj.apps.googleusercontent.com',
+                clientId: '148525925888-v60jijf8b1sijhti00pg6opcr4ule877.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn : this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             })
         });
     }
 
+    onAuthChange = () => {
+      this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    };
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return <div>Signed In </div>
+        } else {
+            return <div>Defs not signed in</div>
+        }
+    }
+
     render() {
-        return <div>GAUTH</div>
+        return (
+            <div>
+                Google Auth
+                {this.renderAuthButton()}
+            </div>
+        )
     }
 }
 
