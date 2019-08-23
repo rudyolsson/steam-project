9b13bbaa04ac7ed2bfe474e6ace733ec15ac5719
Rudy Olsson <rudyolsson@Rudys-MacBook-Pro.local>
2019-08-23 15:46:01 +0200

    add rtmp server

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { connect} from 'react-redux';
 import { fetchStream } from "../../actions";
+import flv from 'flv.js';
 
 export class StreamShow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
     componentDidMount() {
-        if (!this.props.stream) {
-            this.props.fetchStream(this.props.match.params.id);
+        const { id } = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        // if a new render i.e. new id to props causing a rerender 
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+
+    buildPlayer() {
+        const { id } = this.props.match.params;
+        if(this.player || !this.props.stream) {
+            return;
         }
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render(){
@@ -20,6 +48,7 @@ export class StreamShow extends React.Component {
             const { title, description} = this.props.stream;
             return (
                 <div>
+                    <video ref={this.videoRef} style={{ width: '100%' }} controls={true}/>
                     <h1>{title}</h1>
                     <h5>{description}</h5>
                 </div>
